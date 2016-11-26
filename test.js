54e9835bc651605f4b14697b512fb60f3c3f011e
Aaron Chen <aaronhc@tw.ibm.com>
2016-11-27 00:58:34 +0800

    Initial code

--- test.js
+++ test.js
@@ -9,8 +9,25 @@ test.describe('EasyDriver Test Suite', function() {
   });
 
   test.after(function() {
-     easyd.quit();
-   });
+    easyd.quit();
+  });
+
+  test.it('Test Case: wait(), getRect(), setAttribute(), drawFlyover(), drawArrow()', function(done) {
+    easyd.open('https://jsfiddle.net/9snhn98w/show/');
+    easyd.switchToFrame('//iframe');
+    easyd.click('id=p1');
+    easyd.click('id=p2');
+    easyd.click('id=p3');
+    easyd.getRect('id=p3').then(function (rect) {
+     console.log(rect);
+    });
+    easyd.setAttribute('id=p3', 'title', "ccccc");
+    easyd.drawFlyover('id=p3');
+    easyd.drawArrow('id=p1', 'id=desc');
+    easyd.focus('id=p1');
+    easyd.sleep(2000);
+    done();
+  });
 
   test.it('Test Case: takeElementShot()', function(done) {
     easyd.open('https://www.google.com');
@@ -18,6 +35,7 @@ test.describe('EasyDriver Test Suite', function() {
     easyd.waitForVisible('id=rso');
     easyd.scrollIntoView('(//*[@class="_Gs"])[2]');
     easyd.takeElementShot('id=foot', 'test02.png');
+    easyd.sleep(2000);
     done();
   });
 
@@ -25,8 +43,9 @@ test.describe('EasyDriver Test Suite', function() {
     easyd.open('https://jsfiddle.net/aaronchen/8w0zyuyu/show/');
     easyd.switchToFrame('//iframe');
     easyd.checkAll('//form');
-    easyd.sleep(3000);
+    easyd.sleep(2000);
     easyd.unCheckAll('//form');
+    easyd.sleep(2000);
     done();
   });
 
@@ -37,6 +56,7 @@ test.describe('EasyDriver Test Suite', function() {
     easyd.takeScrollShot('id=c2', 'test02_s2.png');
     easyd.takeScrollShot('id=c3', 'test02_s3.png');
     easyd.takeScrollShot('id=c4', 'test02_s4.png');
+    easyd.sleep(2000);
     done();
   });
 
