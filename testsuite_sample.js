86e971f82b7004ceae0062e1d94fcc2d224f2146
Aaron Chen <aaronhc@tw.ibm.com>
2017-04-13 16:28:03 +0800

    Update EasySuite descriptions

--- testsuite_sample.js
+++ testsuite_sample.js
@@ -2,12 +2,15 @@ const EasyDriver = require('./easy-driver');
 const EasySuite = require('./easy-suite');
 
 const locale = process.env.EASYD_LOCALE || 'en';
+const screen_dir = `testsuite/${locale}`;
+
 const easyd = new EasyDriver({locale: locale});
 const suite = new EasySuite('EasyDriver Test Suite');
 easyd.VERBOSE = false;
 
 suite.before(function() {
   easyd.log("Run Environment Setup Here");
+  easyd.createDirectories(screen_dir);
 });
 
 suite.after(function() {
@@ -17,18 +20,21 @@ suite.after(function() {
 
 suite.testcase("010.010.010", function() {
   easyd.open('https://www.google.com');
+  easyd.takeScreenshot(`${screen_dir}/010.010.010.png`);
   easyd.sleep(2000);
 });
 
 suite.testcase("010.010.020", function() {
   easyd.blank();
   easyd.open('https://www.mobile01.com');
+  easyd.takeScreenshot(`${screen_dir}/010.010.020.png`);
   easyd.sleep(2000);
 });
 
 suite.testcase("010.020.010", function() {
   easyd.blank();
   easyd.open('https://www.nytimes.com');
+  easyd.takeScreenshot(`${screen_dir}/010.020.010.png`);
   easyd.sleep(2000);
 });
 
